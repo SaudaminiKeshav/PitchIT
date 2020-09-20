dab1d8649504b25e6a07ff498794d7a9b136fd2e
Saudamini Keshav <saudamini.keshav@gmail.com>
2020-09-20 11:06:04 -0700

    updated post api route

--- routes/post-api-routes.js
+++ routes/post-api-routes.js
@@ -1,8 +1,8 @@
 var db = require("../models");
 var passport = require("../config/passport");
 
-module.exports = function(app) {
-    app.get("/api/trips", function(req, res) {
+module.exports = function (app) {
+    app.get("/api/trips", function (req, res) {
         var query = {};
         if (req.query.author_id) {
             query.AuthorId = req.query.author_id;
@@ -10,91 +10,93 @@ module.exports = function(app) {
         db.Post.findAll({
             where: query,
             include: [db.Author]
-        }).then(function(dbPost) {
+        }).then(function (dbPost) {
             res.join(dbPost);
         });
     });
 
-    app.get("/api/posts/:id", function(req, res) {
+    app.get("/api/posts/:id", function (req, res) {
         db.Post.findOne({
             where: {
                 id: req.params.id
             },
             include: [db.Author]
-        }).then(function(dbPost) {
+        }).then(function (dbPost) {
             res.json(dbPost);
         });
     });
 
-    app.post("/api/posts", function(req, res) {
-        db.Post.createe(req.body).then(function(dbPost) {
+    app.post("/api/posts", function (req, res) {
+        db.Post.createe(req.body).then(function (dbPost) {
             res.json(dbPost);
         });
     });
 
-    app.delete("/api/posts/:id", function(req, res) {
+    app.delete("/api/posts/:id", function (req, res) {
         db.Post.destroy({
             where: {
                 id: req.params.id
             }
-        }).then(function(dbPost) {
+        }).then(function (dbPost) {
             res.json(dbPost);
         });
     });
 
-    app.put("/api/posts", function(req, res) {
+    app.put("/api/posts", function (req, res) {
         db.Post.update(
             req.body,
             {
                 where: {
                     id: req.body.id
                 }
-            }).then(function(dbPost) {
+            }).then(function (dbPost) {
                 res.json(dbPost);
             });
     });
 
-      // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
-  // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
-  // otherwise send back an error
-  app.post("/api/signup", function(req, res) {
-    db.User.create({
-      email: req.body.email,
-      password: req.body.password
-    })
-      .then(function() {
-        res.redirect(307, "/api/login");
-      })
-      .catch(function(err) {
-        res.status(401).json(err);
-      });
-  });
+    // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
+    // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
+    // otherwise send back an error
+    app.post("/api/signup", function (req, res) {
+        db.User.create({
+            name: req.body.name,
+            number: req.body.number,
+            email: req.body.email,
+            password: req.body.password
+        })
+            .then(function () {
+                res.redirect(307, "/api/login");
+            })
+            .catch(function (err) {
+                res.status(401).json(err);
+            });
+    });
 
     // Using the passport.authenticate middleware with our local strategy.
-  // If the user has valid login credentials, send them to the members page.
-  // Otherwise the user will be sent an error
-  app.post("/api/login", passport.authenticate("local"), function(req, res) {
-    res.json(req.user);
-  });
+    // If the user has valid login credentials, send them to the members page.
+    // Otherwise the user will be sent an error
+    app.post("/api/login", passport.authenticate("local"), function (req, res) {
+        res.json(req.user);
+    });
 
     // Route for logging user out
-    app.get("/logout", function(req, res) {
+    app.get("/logout", function (req, res) {
         req.logout();
         res.redirect("/");
-      });
-    
-        // Route for getting some data about our user to be used client side
-  app.get("/api/user_data", function(req, res) {
-    if (!req.user) {
-      // The user is not logged in, send back an empty object
-      res.json({});
-    } else {
-      // Otherwise send back the user's email and id
-      // Sending back a password, even a hashed password, isn't a good idea
-      res.json({
-        email: req.user.email,
-        id: req.user.id
-      });
-    }
-  });
+    });
+
+    // Route for getting some data about our user to be used client side
+    app.get("/api/user_data", function (req, res) {
+        if (!req.user) {
+            // The user is not logged in, send back an empty object
+            res.json({});
+        } else {
+            // Otherwise send back the user's email and id
+            // Sending back a password, even a hashed password, isn't a good idea
+            res.json({
+                email: req.user.email,
+                id: req.user.id
+            });
+        }
+    });
 };
